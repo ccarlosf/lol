f2c2d84910ba30c44f4d1378e1a8794b04089640
ccarlos <2238513100@qq.com>
2019-12-26 22:15:11 +0800

    (9 2-1) 英雄联盟小游戏的最简单版本

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import awkward.hero.Diana;
+
+import java.util.Scanner;
+
 public class Main {
 
+    /**
+     * @description: 主函数入口
+     * @author: ccarlos
+     * @date: 2019/12/26 22:08
+     * @param: args
+     * @return: void
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String name = Main.getPlayerInput();
+        if (name.equals("Diana")) {
+            Diana diana = new Diana();
+            diana.r();
+        }
+    }
+
+    /**
+     * @description: 获取输入英雄名字
+     * @author: ccarlos
+     * @date: 2019/12/26 22:09
+     * @return: java.lang.String
+     */
+    private static String getPlayerInput() {
+        System.out.println("Enter a Hero's Name:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
---
+++ src/awkward/hero/Diana.java
@@ -0,0 +1,45 @@
+package awkward.hero;
+
+/**
+ * @description: 英雄皎月 戴安娜
+ * @author: Created by ccarlos
+ * @date: 2019/12/26 22:04
+ */
+public class Diana {
+
+    /**
+     * @description: Diana技能Q
+     * @author: ccarlos
+     * @date: 2019/12/26 22:07
+     */
+    public void q() {
+        System.out.println("Diana Q");
+    }
+
+    /**
+     * @description: Diana技能W
+     * @author: ccarlos
+     * @date: 2019/12/26 22:07
+     */
+    public void w() {
+        System.out.println("Diana W");
+    }
+
+    /**
+     * @description: Diana技能E
+     * @author: ccarlos
+     * @date: 2019/12/26 22:07
+     */
+    public void e() {
+        System.out.println("Diana E");
+    }
+
+    /**
+     * @description: Diana技能R
+     * @author: ccarlos
+     * @date: 2019/12/26 22:07
+     */
+    public void r() {
+        System.out.println("Diana R");
+    }
+}
